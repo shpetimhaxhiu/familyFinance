8331ef5830f4bbf7e3088ae73263a6195cc6723f
Pito Axiu <shpetim.h@gmail.com>
2018-04-08 13:36:11 +0100

    [change] seperated the code to have more mvc model

---
+++ include/globals.php
@@ -0,0 +1,8 @@
+<?php
+$app = 'Family Finance';
+$version = 0.1;
+
+global $app, $version;
+
+
+?>
--- index.php
+++ index.php
@@ -1,19 +1,34 @@
 <?php
 require_once('classes/Database.php');
+include('header.php');
 
 $db = new Database('localhost', 'root', 'njeriop123!@#', 'familyfinance');
-
 $db->query("select * from ledger");
-if($db->numRows() == 0) {
-  echo 'No entries';
-} else {
-  foreach($db->entries() as $article) {
-    echo $article["EntryDate"], "<br>";
-  }
-  echo $db->numRows(), " entries";
-}
+?>
 
-$db->addEntry("INSERT into Ledger (EntryDate, EntryType) VALUES ('2018-03-03', 'Test 3')");
+<main role="main" class="container">
+
+  <div class="starter-template">
+  <?php  if($db->numRows() == 0) {
+      echo 'No entries';
+    } else {
+      foreach($db->entries() as $article) { ?>
+
+        <p class="lead"><?php echo $article["EntryDate"], "<br>" ?> </p>
+
+    <?php    }
+      echo $db->numRows(), " entries";
+    } ?>
+  </div>
+
+</main><!-- /.container -->
+
+
+
+<?php
+// $db->addEntry("INSERT into Ledger (EntryDate, EntryType) VALUES ('2018-03-03', 'Test 3')");
 
 $db->disconnect();
+
+include('footer.php');
 ?>
